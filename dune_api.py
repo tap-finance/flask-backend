d6121765a5da4617774c6af6f73c6968cfe08165
Efrainq07 <efrainq07@hotmail.com>
2022-11-05 20:52:15 -0600

    [Backend][Flask] Added CORS support.

--- dune_api.py
+++ dune_api.py
@@ -1,10 +1,14 @@
 from flask import Flask
 import requests
 from dune_query_utils import execute_query, get_query_status, get_query_results, query_request_start
+from flask_cors import CORS, cross_origin
 
 app = Flask(__name__)
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
 
 @app.route("/sudoswap_analytics_24hr", methods= ['GET'])
+@cross_origin()
 def sudoswap_volume():
     query_id = '1531117'
     
@@ -13,6 +17,7 @@ def sudoswap_volume():
     return data.content
         
 @app.route("/sudoswap_analytics_tvl", methods= ['GET'])
+@cross_origin()
 def sudoswap_tvl():
     query_id = '1532688'
     
